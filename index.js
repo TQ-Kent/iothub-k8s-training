557298a92e20074d53e0ea464ae1b0ab5cc4ecfc
TQ-Kent <thanhquan.kent@gmail.com>
2025-08-02 17:20:33 +0700

    Commit

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const mqtt = require('mqtt');
+const mqtt = require("mqtt");
 
 // Gets the credentials for connecting the IoTHub service
 const esServices = JSON.parse(process.env.ENSAAS_SERVICES);
-const rabbitmqCreds = esServices['p-rabbitmq'][0].credentials;
+const rabbitmqCreds = esServices["p-rabbitmq"][0].credentials;
 const mqttUri = rabbitmqCreds.protocols.mqtt.uri;
 
 /**
  * Connects to IotHub and Subcribes to the topic when the connection is made.
  */
 const client = mqtt.connect(mqttUri);
-const topic = 'greeting/sk/chen';     // Sets the topic to subscribe -> greeting/firsName/lastName
+const topic = "greeting/phan/quan"; // Sets the topic to subscribe -> greeting/firsName/lastName
 
-client.on('connect', (connack) => {
-  logger('Connected to IoTHub');
+client.on("connect", (connack) => {
+  logger("Connected to IoTHub");
 
   client.subscribe(topic, (err, granted) => {
     if (!err) logger(`Subscribed to ${topic}`);
@@ -22,10 +22,10 @@ client.on('connect', (connack) => {
 /**
  * Receives messages from the above topic. Publishes the message to another topic right away upon receiving it.
  */
-client.on('message', (topic, message, packet) => {
+client.on("message", (topic, message, packet) => {
   const msg = `Hey, I hear you say '${message.toString()}' on topic '${topic}'.`;
 
-  client.publish('blackhole', msg, { qos: 1 }, (err, packet) => {
+  client.publish("blackhole", msg, { qos: 1 }, (err, packet) => {
     if (!err) logger(`Message sent: ${msg}`);
   });
 });
@@ -33,13 +33,13 @@ client.on('message', (topic, message, packet) => {
 /**
  * Other MQTT events
  */
-client.on('error', err => logger(err));
-client.on('close', () => logger('[MQTT]: close'));
-client.on('offline', () => logger("[MQTT]: offline"));
+client.on("error", (err) => logger(err));
+client.on("close", () => logger("[MQTT]: close"));
+client.on("offline", () => logger("[MQTT]: offline"));
 
 /**
  * Helper function
  */
 function logger(s) {
-  console.log(Date() + ' -- ' + s);
+  console.log(Date() + " -- " + s);
 }
